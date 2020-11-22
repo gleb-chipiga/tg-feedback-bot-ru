5896a3e6fb9b1bd5e2512c29d319005aefd3f27e
Gleb Chipiga <gleb.chipiga+git@pm.me>
2020-11-22 18:14:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     ],
     packages=['feedback_bot'],
     python_requires='>=3.8,<3.10',
-    install_requires=['aiotgbot[sqlite]>=0.4.0.dev3,<0.6.0', 'aiojobs',
+    install_requires=['aiotgbot[sqlite]>=0.5.3,<0.6.0', 'aiojobs',
                       'aiojobs-stubs>=0.2.2.post1', 'attrs', 'uvloop'],
     entry_points={
         'console_scripts': [
